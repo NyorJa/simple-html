0ca49b7237b70a19dbe5ca75fd49346a9c343013
Rod Fetalvero <botsot.felix@gmail.com>
2023-02-04 20:34:08 +0800

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+## Instruction
+1. install node js
+2. execute `npm start`
+3. go to your browser and type `localhost:5000`
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Static Files
+app.use(express.static('public'));
+// Specific folder example
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/images'))
+
+// Set View's
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// Navigation
+app.get('', (req, res) => {
+    res.render('index', { text: 'This is EJS'})
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { text: 'About Page',
+address: "hell"})
+})
+
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
